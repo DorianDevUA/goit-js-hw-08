8a188ed77230b1c7b6a0066236dcc407f68ff476
DorianDevUA <dorian.devua@gmail.com>
2024-03-17 22:35:28 +0400

    Adds the code for the second task from Homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+initTime();
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  const { seconds } = data;
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+
+function initTime() {
+  let savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedTime) {
+    savedTime = JSON.parse(savedTime);
+    player.setCurrentTime(savedTime);
+  }
+}
