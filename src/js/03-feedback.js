60be9f273a0383f16267c6d0acc6861e0420c29f
DorianDevUA <dorian.devua@gmail.com>
2024-03-17 16:26:23 +0400

    Adds some code up to the 3rd task of homework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('.feedback-form input[name="email"]'),
+  messageInput: document.querySelector(
+    '.feedback-form textarea[name="message"]'
+  ),
+};
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? {};
+
+refs.feedbackForm.addEventListener('submit', onFormSubmit);
+refs.feedbackForm.addEventListener('input', throttle(onFormInput, 500));
+
+populateData();
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  formData.email = '';
+  formData.message = '';
+}
+
+function populateData() {
+  const savedFormData = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+    refs.emailInput.value = parsedFormData.email || '';
+    refs.messageInput.value = parsedFormData.message || '';
+  }
+}
